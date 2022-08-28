8ee71b35a358d3cb781df725b685da19e9c8603c
IAS <ias@brainbuilders.nl>
2022-08-28 10:11:40 +0100

    Tweak build script for RaspberryPi4 with OpenCV4

--- example/ncnn/build.sh
+++ example/ncnn/build.sh
@@ -1,1 +1,1 @@
-g++ -o FastestDet FastestDet.cpp -I include/ncnn lib/libncnn.a `pkg-config --libs --cflags opencv` -fopenmp
+g++ -O3 -flto -march=native -o FastestDet FastestDet.cpp -I /usr/local/include/ncnn /usr/local/lib/aarch64-linux-gnu/libncnn.a `pkg-config --libs --cflags opencv4` -fopenmp
